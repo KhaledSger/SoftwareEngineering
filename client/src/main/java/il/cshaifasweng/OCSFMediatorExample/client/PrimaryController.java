0c533d16de4d0c53e2402eb45bb4c65559876780
hakee <hakeemabushqara7@gmail.com>
2022-01-13 21:57:04 +0200

    NEWWW

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PrimaryController.java
@@ -2,10 +2,10 @@ package il.cshaifasweng.OCSFMediatorExample.client;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,46 +26,46 @@ public class PrimaryController {
 	@FXML
 	private TextField ID;
 
-// 	@FXML // fx:id="otolaryngology_button"
-// 	private MenuItem otolaryngology_button; // Value injected by FXMLLoader
+ 	@FXML // fx:id="otolaryngology_button"
+ 	private MenuItem otolaryngology_button; // Value injected by FXMLLoader
 
-// 	@FXML // fx:id="skin_button"
-// 	private MenuItem skin_button; // Value injected by FXMLLoader
+ 	@FXML // fx:id="skin_button"
+ 	private MenuItem skin_button; // Value injected by FXMLLoader
 
-// 	@FXML // fx:id="specialized_button"
-// 	private MenuItem specialized_button; // Value injected by FXMLLoader
-// 
-// 	@FXML
-// 	void view_clinics(ActionEvent event){
-// 		try {
-// 			App.setRoot("secondary");
-// 		}catch (IOException e){
-// 			e.printStackTrace();
-// 		}
-// 	}
-// 	@FXML
-// 	void otolaryngology_handler(ActionEvent event) {
+ 	@FXML // fx:id="specialized_button"
+ 	private MenuItem specialized_button; // Value injected by FXMLLoader
 
-// 	}
+ 	@FXML
+ 	void view_clinics(ActionEvent event){
+ 		try {
+ 			App.setRoot("secondary");
+ 		}catch (IOException e){
+ 			e.printStackTrace();
+ 		}
+ 	}
+ 	@FXML
+ 	void otolaryngology_handler(ActionEvent event) {
 
-// 	@FXML
-// 	void reserve_specialization_handler(ActionEvent event) {
+ 	}
 
-// 	}
+ 	@FXML
+ 	void reserve_specialization_handler(ActionEvent event) {
 
-// 	@FXML
-// 	void skin_handler(ActionEvent event) {
-// 		try {
-// 			App.setRoot("secondary");
-// 		}catch (IOException e){
-// 			e.printStackTrace();
-// 		}
+ 	}
 
-// 	}
+ 	@FXML
+ 	void skin_handler(ActionEvent event) {
+ 		try {
+			 SimpleClient.getClient().sendToServer("#getALLClinics");
+ 			App.setRoot("secondary");
+ 		}catch (IOException e){
+ 			e.printStackTrace();
+ 		}
 
- @FXML
- private PasswordField Password;
+ 	}
 
+	@FXML
+	private PasswordField Password;
 
   
 	@FXML
